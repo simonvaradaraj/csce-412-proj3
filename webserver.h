ee39885dbc15ce4b1b8cb83d9d7fbf93c7c8db17
simonvaradaraj <s.varadaraj03@gmail.com>
2024-10-13 15:12:10 -0500

    webserver hedaer file completed

---
+++ webserver.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include "request.h"
+
+using namespace std;
+
+class webserver {
+public:
+    webserver();
+    webserver(string name);
+    request getRequest();
+    string getName();
+    void processRequest(request req, int currentTime);
+    void setName(string name);
+    bool isDone(int currentTime);
+private:
+    request req;
+    int start_time;
+    string name;
+};
