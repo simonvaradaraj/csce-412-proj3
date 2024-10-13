f0c53c69db2b530a56032c37f02ced1479ca5498
simonvaradaraj <s.varadaraj03@gmail.com>
2024-10-13 18:17:02 -0500

    created function to create random requests

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <ctime>
 #include <stdlib.h>
+#include <sstream>
 
 #include "webserver.h"
 #include "loadbalancer.h"
 #include "request.h"
 
 using namespace std;
+
+const int MAX_REQUESTS = 5;
+
+request generateRequest() {
+    stringstream ipIn, ipOut;
+    ipIn << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256);
+    ipOut << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256);
+    request req(ipIn.str(), ipOut.str(), (rand() % 10) + 1, (rand() % 2) == 0 ? 'P' : 'S');
+}
 
 int main()
 {
