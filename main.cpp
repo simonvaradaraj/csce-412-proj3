72b4a5bc8154c8e2121a66b040079c932c87ce42
simonvaradaraj <s.varadaraj03@gmail.com>
2024-10-20 14:01:44 -0500

    added server autoscaling functionality

--- main.cpp
+++ main.cpp
@@ -2,22 +2,23 @@
 #include <ctime>
 #include <stdlib.h>
 #include <sstream>
-
+#include <vector>
 #include "webserver.cpp"
 #include "loadbalancer.cpp"
 #include "request.cpp"
 
 using namespace std;
 
-// 10 servers
-const int NUM_SERVERS = 10;
+// Define thresholds for scaling up/down
+const int INITIAL_NUM_SERVERS = 10;   
+const int MIN_NUM_SERVERS = 4;   
+const int REQUEST_BURST = 1000;   
 
 request generateRequest() {
     stringstream ipIn, ipOut;
     ipIn << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256);
     ipOut << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256) << "." << (rand() % 256);
 
-    // range for task time is 1-10
     request req(ipIn.str(), ipOut.str(), (rand() % 10) + 1, (rand() % 2) == 0 ? 'P' : 'S');
     return req;
 }
@@ -26,9 +27,8 @@ int main() {
     srand(time(NULL));
     loadbalancer balancer;
 
-
     // generate a full queue (usually servers * 100)
-    for (int i = 0; i < (NUM_SERVERS * 100); i++) {
+    for (int i = 0; i < INITIAL_NUM_SERVERS * 100; i++) {
         request req = generateRequest();
         balancer.addRequest(req);
     }
@@ -36,28 +36,54 @@ int main() {
     // Show the starting queue size.
     cout << "Initial queue size: " << balancer.getSize() << endl;
 
+    // Replacing with a vector so i can resize it
+    vector<webserver> serverarray;
 
-    webserver serverarray[NUM_SERVERS];
-    for (int i = 0; i < NUM_SERVERS; i++) {
+    // Start with the initial number of servers
+    for (int i = 0; i < INITIAL_NUM_SERVERS; i++) {
         stringstream name;
         name << "Server " << i+1;
-        serverarray[i] = webserver(name.str());
-        serverarray[i].processRequest(balancer.getRequest(), balancer.getSystemTime());
+        webserver server(name.str());
+        server.processRequest(balancer.getRequest(), balancer.getSystemTime());
+        serverarray.push_back(server);
     }
 
-    // Running for 10000 clock cycles 
+    // Running for 10000 clock cycles
     while (balancer.getSystemTime() < 10000) {
         // cout << "Time " << balancer.getSystemTime() << endl;
         int currentTime = balancer.getSystemTime();
 
-        if (serverarray[currentTime % NUM_SERVERS].isDone(currentTime)) {
-            request req = serverarray[currentTime % NUM_SERVERS].getRequest();
-            if (req.jobType != ' ') {
-                cout << "Time " << currentTime << ": " "Request from " << req.ipIn << " to " << req.ipOut << " of type " << req.jobType << " has been processed by " << serverarray[currentTime % NUM_SERVERS].getName() << endl;
-                serverarray[currentTime % NUM_SERVERS].processRequest(balancer.getRequest(), balancer.getSystemTime());
+        // Check the current queue size
+        int queueSize = balancer.getSize();
+        int currentNumServers = serverarray.size();
+
+        // Scaling up logic
+        if (queueSize > currentNumServers * 10) {
+            int newServers = currentNumServers;  // make the same amount of newservers as the existing ones
+            for (int i = 0; i < newServers; i++) {
+                stringstream name;
+                name << "Server " << serverarray.size() + 1;
+                webserver newServer(name.str());
+                newServer.processRequest(balancer.getRequest(), balancer.getSystemTime());
+                serverarray.push_back(newServer);
             }
-            else {
-                serverarray[currentTime % NUM_SERVERS].processRequest(balancer.getRequest(), balancer.getSystemTime());
+            cout << "Time " << currentTime << ": Doubled servers to " << serverarray.size() << endl;
+        }
+
+        // scale down logic
+        else if (queueSize < currentNumServers && currentNumServers > MIN_NUM_SERVERS) {
+            serverarray.resize(currentNumServers / 2);
+            cout << "Time " << currentTime << ": Halved servers to " << serverarray.size() << endl;
+        }
+
+        // Process each server
+        for (long unsigned int i = 0; i < serverarray.size(); i++) {
+            if (serverarray[i].isDone(currentTime)) {
+                request req = serverarray[i].getRequest();
+                if (req.jobType != ' ') {
+                    cout << "Time " << currentTime << ": Request from " << req.ipIn << " to " << req.ipOut << " of type " << req.jobType << " has been processed by " << serverarray[i].getName() << endl;
+                }
+                serverarray[i].processRequest(balancer.getRequest(), balancer.getSystemTime());
             }
         }
 
@@ -66,11 +92,20 @@ int main() {
             request req = generateRequest();
             balancer.addRequest(req);
             // cout << "Queue size: " << balancer.getSize() << endl;
+            cout << "Time " << currentTime << ": Request from " << req.ipIn << " to " << req.ipOut << " of type " << req.jobType << " has been generated" << endl;
         }
 
+        // Simulate a burst of requests at rare times
+        if ((rand() % REQUEST_BURST) == 0) {
+            cout << "Time " << currentTime << ": Request Burst has been generated" << endl;
+             for (long unsigned int i = 0; i < REQUEST_BURST; i++) {
+                request req = generateRequest();
+                balancer.addRequest(req);
+            }
+        }
         balancer.incTime();
     }
 
-    // Show the starting queue size.
+    // Show the ending queue size
     cout << "Ending queue size: " << balancer.getSize() << endl;
 }
